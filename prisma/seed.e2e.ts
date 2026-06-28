907a7b8cfe5982874b2357b1c15c00a90bb267e8
Joa0V <joao.vitortnt5@gmail.com>
2026-06-28 20:53:01 -0300

    test(e2e): Modificando seed.e2e.ts

--- prisma/seed.e2e.ts
+++ prisma/seed.e2e.ts
@@ -82,6 +82,7 @@ async function main() {
     where: { email: "joao@seed.com" },
     update: {},
     create: {
+      id: "aluno-joao",
       nome: "João Silva",
       nickname: "joaos",
       email: "joao@seed.com",
